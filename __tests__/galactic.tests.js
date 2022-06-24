ce116b4a021896913c6c99e25704ac18eeca9cca
Donovan Weber <donovanweber03@gmail.com>
2022-06-24 12:44:25 -0400

    Red phase for age on mars

--- __tests__/galactic.tests.js
+++ __tests__/galactic.tests.js
@@ -9,11 +9,13 @@ test('should create an age object with two properties of age and life expectancy
     expect(donovan.earthAge).toEqual(19);
     expect(donovan.lifeExpectancy).toEqual(79);
   });
-test('should return the age of the object on mercury', () => {
+test('should return the age of the object on Mercury', () => {
     expect(donovan.ageOnMercury()).toEqual(79.17);
   });
 test('should return the age of the object on Venus', () => {
     expect(donovan.ageOnVenus()).toEqual(30.65);
   });
-
+test('should return the age of the object on Mars', () => {
+    expect(donovan.ageOnMars()).toEqual();
+  });
 });
